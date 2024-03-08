0a0771f10190d3be4909088f857327fe192be4a7
Kevin Wessa <kjwessa@gmail.com>
2024-03-08 14:36:41 -0600

    feat: add static friends list with initial data

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import { FriendsList } from "./FriendsList";
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
+  );
+}
