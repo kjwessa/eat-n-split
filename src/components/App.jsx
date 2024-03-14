74fb6b8f36a0c77b900681f21f145d5dc27f0cdc
Kevin Wessa <kjwessa@gmail.com>
2024-03-14 10:32:07 -0500

    feat: add form to split the bill with a friend

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import FormAddFriend from "./FormAddFriend";
 import FriendsList from "./FriendsList";
 import Button from "./Button";
+import FormSplitBill from "./FormSplitBill";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <FormAddFriend />
         <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
