870b59c5ea9402f05516b4639a7c4d5de92fb5f4
Kevin Wessa <kjwessa@gmail.com>
2024-03-21 08:33:02 -0400

    feat: add button component and conditional rendering on FormAddFriend component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,21 @@ import FormAddFriend from "./FormAddFriend";
 import FriendsList from "./FriendsList";
 import Button from "./Button";
 import FormSplitBill from "./FormSplitBill";
+import { useState } from "react";
 
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <FormAddFriend />
-        <Button>Add Friend</Button>
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleShowAddFriend}>{showAddFriend ? "Close" : "Add Friend"}</Button>
       </div>
       <FormSplitBill />
     </div>
